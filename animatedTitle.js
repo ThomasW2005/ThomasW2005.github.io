77999fdc7948ad686ef32f1efd75dcf66015dfe9
Thomas Weichhart <multihacker007yt@gmail.com>
2021-07-23 18:43:17 +0200

    updated scrolling title text

--- animatedTitle.js
+++ animatedTitle.js
@@ -1,14 +1,8 @@
 function animateTitle(Title = "Hello, World!", delay = 300) {
-    let counter = 0;
-    let direction = true;
+    let counter = Title.length;
+    let direction = false;
     aniTitle = setInterval(function () {
-        // if (counter == Title.length)
-        //     direction = false;
-        // if (counter == false)
-        //     direction = true;
-
         direction = counter == Title.length ? false : counter == false ? true : direction
-
         counter = (direction == true) ? ++counter : --counter;
         newtitle = (counter == 0) ? "⠀" : Title.slice(0, counter);
         document.title = newtitle;
