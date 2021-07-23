216a4fd028735f020453aa30606100b7d715e6b1
Thomas Weichhart <multihacker007yt@gmail.com>
2021-07-23 18:39:33 +0200

    updated js

--- animatedTitle.js
+++ animatedTitle.js
@@ -2,10 +2,13 @@ function animateTitle(Title = "Hello, World!", delay = 300) {
     let counter = 0;
     let direction = true;
     aniTitle = setInterval(function () {
-        if (counter == Title.length)
-            direction = false;
-        if (counter == false)
-            direction = true;
+        // if (counter == Title.length)
+        //     direction = false;
+        // if (counter == false)
+        //     direction = true;
+
+        direction = counter == Title.length ? false : counter == false ? true : direction
+
         counter = (direction == true) ? ++counter : --counter;
         newtitle = (counter == 0) ? "⠀" : Title.slice(0, counter);
         document.title = newtitle;
